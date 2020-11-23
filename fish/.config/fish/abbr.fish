7ac15b5d25f7afb72ae7f6692a1ff24661a59164
Nathan Button <nathanebutton@gmail.com>
2020-11-23 02:32:44 -0600

    Change abbr home direcory

--- fish/.config/fish/abbr.fish
+++ fish/.config/fish/abbr.fish
@@ -3,24 +3,23 @@ if status --is-interactive
     abbr --add --global -- :q! exit
     abbr --add --global -- :wq exit
     abbr --add --global -- c clear
-    abbr --add --global -- cc /home/nate601/Courses/CourseSelection.fish
-    abbr --add --global -- cdc 'cd /home/nate601/Courses/'
-    abbr --add --global -- cdd 'cd /home/nate601/Documents'
-    abbr --add --global -- cdl 'cd /home/nate601/localWorkspace'
-    abbr --add --global -- cdp 'cd /home/nate601/Workspace/expenseTrackerCli'
-    abbr --add --global -- cds 'cd /home/nate601'
-    abbr --add --global -- cdw 'cd /home/nate601/Workspace'
+    abbr --add --global -- cc /home/nathan/Courses/CourseSelection.fish
+    abbr --add --global -- cdc 'cd /home/nathan/Courses/'
+    abbr --add --global -- cdd 'cd /home/nathan/Documents'
+    abbr --add --global -- cdl 'cd /home/nathan/localWorkspace'
+    abbr --add --global -- cdp 'cd /home/nathan/Workspace/expenseTrackerCli'
+    abbr --add --global -- cds 'cd /home/nathan'
+    abbr --add --global -- cdw 'cd /home/nathan/Workspace'
     abbr --add --global -- dotnew 'dotnet new'
-    abbr --add --global -- fishconfig 'nvim /home/nate601/.config/fish/config.fish'
     abbr --add --global -- g gvim
     abbr --add --global -- gca 'git commit -a'
     abbr --add --global -- gcam 'git commit -a -m '
     abbr --add --global -- gd 'git difftool'
     abbr --add --global -- gp 'git push'
-    abbr --add --global -- i3config 'nvim /home/nate601/.i3/config'
+    abbr --add --global -- i3config 'nvim /home/nathan/.dotfiles/i3/.config/i3/config'
     abbr --add --global -- l ls
     abbr --add --global -- lg lazygit
-    abbr --add --global -- nvimconfig 'nvim /home/nate601/.config/nvim/init.vim'
+    abbr --add --global -- nvimconfig 'nvim /home/nathan/.dotfiles/nvim/.config/nivm/init.vim'
     abbr --add --global -- pbb 'pb push'
     abbr --add --global -- pbf 'pb push --file'
     abbr --add --global -- pcs 'nvim Program.cs'
