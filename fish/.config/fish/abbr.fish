96aeaf5355aa8e8365da4b38eeee98995d8be51e
Nathan Button <nathanebutton@gmail.com>
2020-11-25 01:42:41 -0600

    Add comments to distingusih commands

--- fish/.config/fish/abbr.fish
+++ fish/.config/fish/abbr.fish
@@ -1,29 +1,38 @@
 if status --is-interactive
+    # Vim-like exit commants
     abbr --add --global -- :q exit
     abbr --add --global -- :q! exit
     abbr --add --global -- :wq exit
+    #  Frequently used command abbreviations (e.g. swiftly clearing the screen)
     abbr --add --global -- c clear
+    abbr --add --global -- l ls
+    abbr --add --global -- .. cd ..
+    # Course selection script to change the current course
     abbr --add --global -- cc /home/nathan/Courses/CourseSelection.fish
+    # Quick movement to frequently used directories
     abbr --add --global -- cdc 'cd /home/nathan/Courses/'
     abbr --add --global -- cdd 'cd /home/nathan/Documents'
-    abbr --add --global -- cdl 'cd /home/nathan/localWorkspace'
     abbr --add --global -- cdp 'cd /home/nathan/Workspace/expenseTrackerCli'
     abbr --add --global -- cds 'cd /home/nathan'
     abbr --add --global -- cdw 'cd /home/nathan/Workspace'
-    abbr --add --global -- dotnew 'dotnet new'
-    abbr --add --global -- g gvim
+    # Git commands
+    abbr --add --global -- gc 'git commit'
     abbr --add --global -- gca 'git commit -a'
     abbr --add --global -- gcam 'git commit -a -m '
     abbr --add --global -- gd 'git difftool'
     abbr --add --global -- gp 'git push'
+    abbr --add --global -- gs 'git status'
+    # Config Files
     abbr --add --global -- i3config 'nvim /home/nathan/.dotfiles/i3/.config/i3/config'
     abbr --add --global -- nvimconfig 'nvim /home/nathan/.dotfiles/nvim/.config/nvim/init.vim'
     abbr --add --global -- fishabbr 'nvim /home/nathan/.dotfiles/fish/.config/fish/abbr.fish'
-    abbr --add --global -- l ls
     abbr --add --global -- pcs 'nvim Program.cs'
     abbr --add --global -- sl 'exa --header --long --git'
+    # Vim shortcuts and re-abbreviations
     abbr --add --global -- v nvim
     abbr --add --global -- vi nvim
     abbr --add --global -- vim nvim
-    abbr --add --global -- .. cd ..
+    abbr --add --global -- g gvim
+    # Abbreviate dotnet project creation
+    abbr --add --global -- dotnew 'dotnet new'
 end
