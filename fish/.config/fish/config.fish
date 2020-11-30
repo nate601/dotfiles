5f5fde6d035050d8e0008ad47e83a68ec157573f
Nathan Button <nathanebutton@gmail.com>
2020-11-30 01:02:30 -0600

    Add go to path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -15,6 +15,13 @@ set -gx NUGET_PACKAGES $XDG_CONFIG_HOME/NuGetPackages
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 
+# go
+
+set -gx GOROOT /usr/local/go
+set -gx GOPATH $HOME/Workspace/goProjects
+
+set PATH $PATH $HOME/.dotfiles/scripts
+
 thefuck --alias | source
 source ~/.config/fish/abbr.fish    
 fish_ssh_agent
